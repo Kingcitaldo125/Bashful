6eb9c09c60e4543b059317db75c366eb365cea4b
Paul <paularelt@gmail.com>
2025-08-15 14:36:54 -0400

    Update sudoshell.sh

--- sudoshell/sudoshell.sh
+++ sudoshell/sudoshell.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Sudoshell will allow a new shell session to preserve your `sudo` credentials
+# This effectvely overrides `sudo`, allowing you to specify `sudo` as a command prefix without re-entering your credentials
+# It does this by prompting you once, upon a new shell session, for your sudo credentials, then caching those credentials in a bash alias
 
-# Add this to your ~/.bashrc
+# Add the following to your ~/.bashrc
 read -s -p "Enter password: " xpasswd
-alias update="sudo yum -y update"
 alias sudo="echo ${xpasswd} | sudo -S ls &>/dev/null && sudo"
 clear
